e452dbcb2b8497c571e654e6f0727134677c3b79
weirb <billyweir1@gmail.com>
2018-06-22 15:41:10 +0100

    Ignore build files from git.

--- src/billy.h
+++
@@ -1,7 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<pml_helmholtz_elements.h> 
-#include<pml_helmholtz_flux_elements.h> 
-#include<refineable_pml_helmholtz_elements.h> 
-#include<complex_smoother.h> 
-#include<helmholtz_geometric_multigrid.h> 
