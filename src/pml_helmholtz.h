e9ec0cb2756036d81a66824e17ecda6e0f2dbc24
Billy Weir <billyweir1@gmail.com>
2018-06-22 14:05:19 +0100

    Add src code for building library.

---
+++ src/pml_helmholtz.h
@@ -0,0 +1,8 @@
+// This file was generated automatically during the make process
+// and it will be remade automatically
+#include<pml_helmholtz/pml_helmholtz_elements.h> 
+#include<pml_helmholtz/pml_helmholtz_flux_elements.h> 
+#include<pml_helmholtz/Tpml_helmholtz_elements.h> 
+#include<pml_helmholtz/refineable_pml_helmholtz_elements.h> 
+#include<pml_helmholtz/complex_smoother.h> 
+#include<pml_helmholtz/helmholtz_geometric_multigrid.h> 
