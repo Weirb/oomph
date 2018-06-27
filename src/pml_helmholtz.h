b96d15491502aa9be78defc2ae96e787cdc4202b
Billy Weir <billyweir1@gmail.com>
2018-06-27 15:27:22 +0100

    Remove broken codes, previous version on alternate branch.

--- src/pml_helmholtz.h
+++
@@ -1,8 +0,0 @@
-// This file was generated automatically during the make process
-// and it will be remade automatically
-#include<pml_helmholtz/pml_helmholtz_elements.h> 
-#include<pml_helmholtz/pml_helmholtz_flux_elements.h> 
-#include<pml_helmholtz/Tpml_helmholtz_elements.h> 
-#include<pml_helmholtz/refineable_pml_helmholtz_elements.h> 
-#include<pml_helmholtz/complex_smoother.h> 
-#include<pml_helmholtz/helmholtz_geometric_multigrid.h> 
