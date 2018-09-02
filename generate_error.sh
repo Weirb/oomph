2f185da2d5bee5d7f49bfbc361b187ad683831fe
William Weir <mbbxwww4@vummath.ma.man.ac.uk>
2018-09-02 13:28:42 +0100

    Update script to generate data.

--- generate_error.sh
+++ generate_error.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
-for k in {1..8}
+MAX_ITER=6
+
+# Parameters we want the solution for
+declare -a ks=(100 0 10 0);
+declare -a ns=(5   0 0  5);
+
+len=$(echo "${#ks[@]} - 1" | bc);
+
+# Loop over the parameters we want to solve for
+for j in `seq 0 $len`;
 do
-	echo "Solving Helmholtz MG $k"
-	main --linear_solver 1 --k_sq 100 --n_fourier 5 --min_ref $k \
-	>> "logs/output_helmholtz_mg_$k.dat"
+	k=${ks[$j]};
+	n=${ns[$j]};
 
-	echo "Solving Helmholtz SuperLU $k"
-	main --linear_solver 0 --k_sq 100 --n_fourier 5 --min_ref $k \
-	>> "logs/output_helmholtz_superlu_$k.dat"
+	echo "Solving problem k^2=$k, N=$n";
+
+	FOLDER="logs/output_k_$k";
+	FOLDER+="_n_$n";
+	echo "Creating folder: $FOLDER";
+	mkdir -p $FOLDER;
+
+	# Compute the SuperLU solution
+	for i in `seq 1 $MAX_ITER`;
+	do
+	
+		echo "Solving SuperLU $i"
+		main --linear_solver 0 --k_sq $k --n_fourier $n --min_ref $i \
+		>> "$FOLDER/superlu_$i.dat"
+	done
+	
+	# Compute the MG solution
+	for i in `seq 1 $MAX_ITER`;
+	do
+		echo "Solving MG $i"
+		main --linear_solver 1 --k_sq $k --n_fourier $n --alpha 0.5 --min_ref $i \
+		>> "$FOLDER/mg_$i.dat"
+	done
+	echo;
 done
-
