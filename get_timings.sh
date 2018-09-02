28343ac9a609ef8c72b9a89a23871a4ffb17e3d1
Weirb <billyweir1@gmail.com>
2018-09-02 13:28:42 +0100

    Update script to generate data.

--- get_timings.sh
+++ get_timings.sh
@@ -1,56 +1,76 @@
 #!/bin/bash
 
-kmax=6
+MAX_ITER=6
 
-# Get all of the multigrid data first
-for ((k=1; k<=$kmax; k++))
+# Parameters we want the solution for
+declare -a ks=(100 0 10 0);
+declare -a ns=(5   0 0  5);
+
+len=$(echo "${#ks[@]} - 1" | bc);
+
+# Loop over the parameters we want to solve for
+for j in `seq 0 $len`;
 do
-	echo "Multigrid $k"
+	k=${ks[$j]};
+	n=${ns[$j]};
 
-	h=`echo "scale=5;2^(1-$k)" | bc -l`
-	file="logs/output_helmholtz_mg_$k.dat"
+	FOLDER="logs/output_k_$k";
+	FOLDER+="_n_$n";
 
-	# Get the error from each log
-	# Only need to get this once for mg, not for superlu
-	grep "Norm of error" $file \
-	| awk -v z="$h" 'BEGIN{FS=":";ORS="\n"} {print z,$NF}' \
-	>>"data/helmholtz_error.dat"
+	OUT_FOLDER="data/output_k_$k";
+	OUT_FOLDER+="_n_$n";
+	mkdir -p $OUT_FOLDER;
 
-	# Get the time from the logs
-	TIME=$(grep "Total time for linear solver" $file \
-	| awk '{print $NF}')
+	# Get all of the multigrid data first
+	for ((i=1; i<=$MAX_ITER; i++))
+	do
+		echo "Multigrid $i"
 	
-	# Get the number of dofs from the logs
-	DOFS=$(grep "Total time for linear solver" $file \
-	| sed -r 's/[0-9]+\.[0-9]+//g' \
-	| sed -r 's/[^0-9]*//g')
+		h=`echo "scale=5;2^(1-$i)" | bc -l`
+		file="$FOLDER/mg_$i.dat"
 	
-	echo $TIME $DOFS >>"data/helmholtz_mg_time.dat"
-
-	# Get the number of iterations for FGMRES
-	# This only applies to multigrid
-	ITERATIONS=$(grep "Number of iterations to converge" $file \
-	| sed -r 's/[^0-9]//g')
-
-	echo $ITERATIONS $DOFS >>"data/helmholtz_mg_iter.dat"
-done
-
-# Now get the SuperLU data
-for ((k=1; k<=$kmax; k++))
-do
-	echo "SuperLU $k"
-
-	h=`echo "scale=5;2^(1-$k)" | bc -l`
-	file="logs/output_helmholtz_superlu_$k.dat"
-
-	# Get the time from the logs
-	TIME=$(grep "Total time for linear solver" $file \
-	| awk '{print $NF}')
+		# Get the error from each log
+		# Only need to get this once for mg, not for superlu
+		grep "Norm of error" $file \
+		| awk -v z="$h" 'BEGIN{FS=":";ORS="\n"} {print z,$NF}' \
+		>>"$OUT_FOLDER/error.dat"
 	
-	# Get the number of dofs from the logs
-	DOFS=$(grep "Total time for linear solver" $file \
-	| sed -r 's/[0-9]+\.[0-9]+//g' \
-	| sed -r 's/[^0-9]*//g')
+		# Get the time from the logs
+		TIME=$(grep "Total time for linear solver" $file \
+		| awk '{print $NF}')
+		
+		# Get the number of dofs from the logs
+		DOFS=$(grep "Total time for linear solver" $file \
+		| sed -r 's/[0-9]+\.[0-9]+//g' \
+		| sed -r 's/[^0-9]*//g')
+		
+		echo $TIME $DOFS >>"$OUT_FOLDER/mg_time.dat"
 	
-	echo $TIME $DOFS >>"data/helmholtz_superlu_time.dat"
+		# Get the number of iterations for FGMRES
+		# This only applies to multigrid
+		ITERATIONS=$(grep "Number of iterations to converge" $file \
+		| sed -r 's/[^0-9]//g')
+	
+		echo $ITERATIONS $DOFS >>"$OUT_FOLDER/mg_iter.dat"
+	done
+	
+	# Now get the SuperLU data
+	for ((i=1; i<=$MAX_ITER; i++))
+	do
+		echo "SuperLU $i"
+	
+		h=`echo "scale=5;2^(1-$i)" | bc -l`
+		file="$FOLDER/superlu_$i.dat"
+	
+		# Get the time from the logs
+		TIME=$(grep "Total time for linear solver" $file \
+		| awk '{print $NF}')
+		
+		# Get the number of dofs from the logs
+		DOFS=$(grep "Total time for linear solver" $file \
+		| sed -r 's/[0-9]+\.[0-9]+//g' \
+		| sed -r 's/[^0-9]*//g')
+		
+		echo $TIME $DOFS >>"$OUT_FOLDER/superlu_time.dat"
+	done
 done
